5c07c6cb29d7ad8ca92510b72c6b854abf0b55c2
achraf799 <lm_boudiaf@esi.dz>
2025-02-12 18:01:09 +0100

    building the SEO9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -200,6 +200,8 @@ function App() {
       <h1>dentiste setif</h1>
       <h1>dentiste Algerie</h1>
       <h1>dentiste N'gaous</h1>
+      <h1>dentiste Ngaous</h1>
+      <h1>dentiste Ngaous Batna</h1>
       <h1>dentiste Biskra</h1>
       <h1>dentiste Batna</h1>
       <h1>dentiste Khenchlaa</h1>
@@ -208,6 +210,9 @@ function App() {
       <h1>dentiste ngaous </h1>
       <h1>dentiste Batna</h1>
       <h1>dentiste M'sila</h1>
+      <h1>dentiste Msila</h1>
+      <h1>dentiste Alger</h1>
+      <h1>dentiste Algerie</h1>
       <h1>soins dentaires</h1>
       <h1>orthodontie, implants, esthétique dentaire, chirurgie orale, blanchiment, hygiène dentaire, urgence dentaire, Algérie</h1></div>
         <Navbar />
